21cfba4d0ffbabb2d4007ff6f7dca3f1c802c327
Jane Adam <jane.adam.inbox@gmail.com>
2024-07-28 01:55:26 +0100

    changed netlify dir

--- netlify/functions/lambdaServer.js
+++ netlify/functions/lambdaServer.js
@@ -1,4 +1,4 @@
 const serverless = require(serverless-http);
-const lambdaServer = require('../src/newServer');
+const lambdaServer = require('../../src/newServer');
 
 module.exports.handler(serverless(lambdaServer));
