d9ca1e09d28fc6fe9d5a04b6fa8494ee44086804
Jane Adam <jane.adam.inbox@gmail.com>
2024-07-28 04:51:26 +0100

    reintroduced create-payment-intent

--- netlify/functions/lambdaServer.ts
+++ netlify/functions/lambdaServer.ts
@@ -1,15 +1,15 @@
 // Imports
-import express, { Router, Express } from "express";
+import express, { Router, Express, Request, Response } from "express";
 import bodyParser from "body-parser";
-import stripePackage from "stripe";
+import stripePackage, { Stripe } from "stripe";
 import cors from "cors";
 import serverless from "serverless-http";
 
-// Declaration of server
+// Declaration of express server & router
 const lambdaServer: Express = express();
 const router: Router = Router();
 
-//middleware
+// Middleware
 lambdaServer.use(
   cors({
     origin: "*", // Currently a wildcard. Can be replaced with client origin
@@ -17,19 +17,45 @@ lambdaServer.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-lambdaServer.use(bodyParser.json());
 
-// router
-router.get("/", (req, res) => {
+// GET for troubleshooting
+router.get("/", (req: Request, res: Response): void => {
   res.send("Hello from lambdaServer!");
 });
-router.get("/2", (req, res) => {
-    res.send("Hello from second example!");
-  });
-// Working, but don't understand why this is necessary
+
+// Payment Intents
+const secretStripeKey: string =
+  "sk_test_51PKfLNRwMlFpLQQhlspLqt0zpDLuALXbGrGC9PLe38SmLn7FvOf0V5GE9D2DZWKRkQWzfAgJcKoULY2RBcKy7e5k00EsCsvTQM";
+const stripe: stripePackage = new stripePackage(secretStripeKey, {
+  apiVersion: "2024-04-10",
+});
+async function createPaymentIntent(req: Request, res: Response) {
+  const { amount }: { amount: number } = req.body;
+  console.log(`received createPaymentIntent request; amount: ${amount}`);
+  try {
+    const paymentIntent: Stripe.PaymentIntent =
+      await stripe.paymentIntents.create({
+        amount,
+        currency: "eur",
+      });
+    if (paymentIntent?.client_secret) {
+      console.log(
+        `client_secret extant, typeof: ${typeof paymentIntent?.client_secret}`
+      );
+    }
+    res.status(200).send({ client_secret: paymentIntent.client_secret });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+}
+router.post(
+  "/create-payment-intent",
+  bodyParser.json({ type: "application/json" }),
+  createPaymentIntent
+);
+
+// First argument refers to where server is LISTENING not posting. Redirection necessary because relative paths ordinarily assume listening at the root of the domain.
 lambdaServer.use("/.netlify/functions/lambdaServer/", router);
 
 //export
 export const handler = serverless(lambdaServer);
-
-
